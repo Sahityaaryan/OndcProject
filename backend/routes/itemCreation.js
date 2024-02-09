f83b449e173eba1d731975f1c6ed55a18bcf9892
yash gupta <yashguupta9798@gmail.com>
2024-02-10 01:15:25 +0530

    routes created

---
+++ backend/routes/itemCreation.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { Items } from "../models/category.model";
+const router=new Router()
+
+router.post("/",async(req,res)=>{
+    // const data = req.body
+    try {
+        const successObject=await Items.create({
+            name:req.body.name,
+            category:req.body.category,
+            description:req.body.description,
+            img_url:req.body.img_url,
+            price:req.body.price,
+            stock:req.body.stock,
+        });
+        res.status(200).json({message:`${req.body.name} item has been listed`,success:true})
+    } catch (error) {
+        res.status(502).json({message:error.message,success:false})
+    }
+})
